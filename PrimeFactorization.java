8f909a4a55e8deb32726fa22c525ee06d1eed84b
minikdj <minikdj11@gmail.com>
2018-03-02 10:33:29 -0500

    Takes in a string from the console, converts it to a big int

---
+++ PrimeFactorization.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class PrimeFactorization {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter an Integer: ");
+		BigInteger number = new BigInteger(in.next());
+		System.out.println(number);
+	}
+}
