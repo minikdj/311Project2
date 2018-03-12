166492cd2bd8d84368ae3748f17a4baeffdbcf02
Leo <Leo@172.25.52.143>
2018-03-12 11:39:08 -0400

    Prime Factorization class completed.

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -1,17 +1,57 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PrimeFactorization {
+	//private static ArrayList<Integer> primes = new ArrayList<Integer>();
+	
+	public static void primeFactorization(BigDecimal num, ArrayList<Integer> primesList) {
+		int maxVal = SquareRoot.sqRoot(num).intValue()+1;
+		for(int i = 2; i < maxVal; i++) {
+			if(num.intValue() % i == 0) {
+				primesList.add(i);
+				BigDecimal factoredNum = new BigDecimal(num.intValue()/i);
+				primeFactorization(factoredNum, primesList);
+				break;
+			} else if (i==maxVal-1) {
+				primesList.add(num.intValue());
+			}	
+		}		
+		//return primes;
+	}
+	
+	public static String printPrimesStr(BigDecimal number, ArrayList<Integer> primesList) {
+		String solution = Integer.toString(number.intValue()) + " := ";
+		for(int i = 0; i < primesList.size(); i++) {
+			int tempInt = primesList.get(i);
+			if(i == primesList.size()-1)
+				solution += Integer.toString(tempInt);
+			else
+				solution += Integer.toString(tempInt) + " * ";
+		}
+		
+		return solution;
+	}
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter an Integer: ");
-		BigDecimal number = new BigDecimal(in.next());
-		System.out.println(number);
-		
-		BigDecimal answer = SquareRoot.sqRoot(number);
-		System.out.println(answer);
+		while(true) {
+			System.out.print("Enter an Integer: ");
+			BigDecimal number = new BigDecimal(in.next());
+			ArrayList<Integer> primes = new ArrayList<Integer>();
+			primeFactorization(number, primes);
+			
+			System.out.printf("%d := ", number.intValue());
+			for(int i = 0; i < primes.size(); i++) {
+				if(i == primes.size()-1)
+					System.out.printf("%d\n", primes.get(i));
+				else
+					System.out.printf("%d * ", primes.get(i));
+			}
+			//System.out.println(printPrimesStr(number, primes));
+			primes.clear();
+		}
+		//in.close();
 	}
 }
